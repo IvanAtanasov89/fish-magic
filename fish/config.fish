91964688e9975338ed27cee8e42266f52eb618da
Ivan Atanasov <ivata89@gmail.com>
2020-01-08 16:09:59 +0000

    new config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,10 @@
 set -x SDKMAN_DIR "$HOME/.sdkman"
 set -x JAVA_HOME "$SDKMAN_DIR/candidates/java/current"
 set -x JRE_HOME "$SDKMAN_DIR/candidates/java/current/jre"
+set -x GRADLE_HOME "$SDKMAN_DIR/candidates/gradle/current"
+set -x SPRING_HOME "$SDKMAN_DIR/candidates/springboot/current"
+set -x MICRONAUT_HOME "$SDKMAN_DIR/candidates/micronaut/current"
 set -x PYENV_ROOT "$HOME/.pyenv"
-set -x PATH $PYENV_ROOT/bin $PYENV_ROOT/shims $JAVA_HOME/bin $JRE_HOME/bin $PATH
+set -x PATH $PYENV_ROOT/bin $PYENV_ROOT/shims $JAVA_HOME/bin $JRE_HOME/bin $GRADLE_HOME/bin $SPRING_HOME/bin $MICRONAUT_HOME/bin $PATH
+set -x GOPATH $HOME/go
+set -x FISH_KUBECTL_COMPLETION_COMPLETE_CRDS 0
